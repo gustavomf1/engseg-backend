28304fa41390bcd2fcd8c8f185ab79f55f67244b
Gustavo Martins França <gustavo_mf1@hotmail.com>
2026-04-15 14:13:45 -0300

    chore: remove unnecessary TestMethodSecurityConfig

--- src/test/java/com/engseg/config/TestMethodSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.engseg.config;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-@TestConfiguration
-@EnableMethodSecurity
-public class TestMethodSecurityConfig {
-}
